1d6d33844dd3e1759fa70c9015ea10554404c17b
unknown <monirweb420@gmail.com>
2023-08-15 22:24:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById('submit').addEventListener( 'click', function(){
+
+
+
+    const emailFild=document.getElementById('username');
+const mail=emailFild.value ;
+
+
+   const passwordFild=document.getElementById('password');
+const userpassword=passwordFild.value ;
+
+
+if(mail==='monirweb420@gmail.com'&&  userpassword==='monir' ){
+    window.location.href='home.html';
+}
+else{
+   alert('ur invalid user please inter your valid user-name and password');
+}
+
+});
